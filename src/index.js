dfa044c9b526d163e87333fad5f9959acaf2205c
jcblw <jacoblowe2.0@gmail.com>
2015-12-05 10:34:39 -0800

    adding in jsdoc documentation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ import path from 'path'
 import {EventEmitter} from 'events'
 import {safeStringify} from './safe-stringify'
 
+/**
+ * create, this will setup this a higher level function that returns a component server
+ *
+ * @param {Object} options - a set of options to configure defaults of server
+ * @param {string} options.componentsDir - the relative path from process.cwd() to components directory
+ * @param {string} options.templatesDir - the relative path from process.cwd() to templates directory
+ * @param {string} options.[server] - a express server object
+ * @param {Object} options.[defaults] - default set of params for paths
+ * @param {string} options.[defaults.component] - the component to use if none is passed
+ * @param {string} options.[defaults.template] - the template to use if none is passed
+ * @param {Object} options.[defaults.params] - the props to use if none is passed
+ * @returns {Object} reactComponentServer - a new instance of a component server
+ */
+
 function create (options = {}) {
   const expressApp = options.server || express()
   const errorMessage = options.errorMessage || 'An error happened'
@@ -15,15 +29,30 @@ function create (options = {}) {
     component: path.resolve(process.cwd(), options.componentsDir || './components'),
     template: path.resolve(process.cwd(), options.templatesDir || './templates')
   }
+
   class ReactComponentServer extends EventEmitter {
+    /**
+     * ::constructor - sets up components server, initializes EventEmitter, and exposes expressApp
+     */
     constructor () {
       super()
       this.expressApp = expressApp // expose this
     }
-    // we should working with gets 99% of the time
+    /**
+     * ::get The only METHOD of express proxied
+     *
+     * @param {string} path - the path to handle when the server get a request to it. eg. "/about"
+     * @param {mixed} args - the rest of the arguments proxied to handleRoute. see for more details
+     */
     get (path, ...args) {
       expressApp.get(path, this.handleRoute(...args))
     }
+    /**
+     * ::renderComponent - takes inputed options validate and then renders out components and places in template sends back to request
+     *
+     * @param  {object} options - an object that holds the config for the request
+     * @param  {mixed} options.[param] - please see defaults for create for more details
+     */
     renderComponent (options, res) {
       this.isValidSetup(options, (err, requires) => {
         if (err) {
@@ -37,6 +66,12 @@ function create (options = {}) {
         res.send(`${doctype}${html}`)
       })
     }
+    /**
+     * ::isValidSetup - checks to see if options passes are valid
+     *
+     * @param {Object} _options - the same options object passed to create
+     * @param  {function} callback - a function to be called once validation is done passes node style (err, results)
+     */
     isValidSetup (_options, callback) {
       // this can be alot stricter
       const options = Object.assign({}, _options, defaults)
@@ -56,6 +91,12 @@ function create (options = {}) {
           callback(err)
         })
     }
+    /**
+     * ::onError - is a method that simplifys error handling inside of components, with show errors on dev, and hide them on prod. Also looks to see if there is a error event binding
+     *
+     * @param {Error} err - an error object
+     * @param {Object} res - an http res object with express sugar
+     */
     onError (err, res) {
       if (this.listeners('error').length) {
         if (res) {
@@ -68,6 +109,12 @@ function create (options = {}) {
       console.error(`Unhandled 'error' event`)
       throw err
     }
+    /**
+     * ::isValidRequire - a method that will check to see if path is currently there before requiring
+     *
+     * @param {string} _path - a path to check stat
+     * @returns {Promise} promise - a new promise object that resolves if valid
+     */
     isValidRequire (_path) {
       return new Promise((resolve, reject) => {
         fs.stat(_path, (err) => {
@@ -78,6 +125,12 @@ function create (options = {}) {
         })
       })
     }
+    /**
+     * ::handleRoute - handles the express route and sets up options for renderComponent
+     *
+     * @param  {Object|Function|undefined} options - can be a number of types resolves to being an object
+     * @param {function|undefined} handler - is a function to call to get/overwrite options
+     */
     handleRoute (options = {}, handler) {
       // handles function second param
       if (typeof options === 'function') {
@@ -93,6 +146,12 @@ function create (options = {}) {
         })
       }
     }
+    /**
+     * listen - a proxy for express's listen method
+     *
+     * @param {number} port - a port number to listen on
+     * @param {Function} [callback] - a callback to call after port is listening
+     */
     listen (port, callback) {
       expressApp.listen(port, callback)
     }
